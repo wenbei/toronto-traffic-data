ab658d81332f0eb80ae45718a5bb1e1e804dcf0d
Wen Bei Li <wenbei999@hotmail.com>
2022-09-14 15:01:25 -0700

    Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [svelte()],
+  publicDir: false,
+  build: {
+    outDir: "public",
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        // assetFileNames: `assets/[name].[ext]`
+        assetFileNames: (assetInfo) => {
+          const fileType = assetInfo.name.split(".").at(1);
+          if (fileType == "css") {
+            return `[name][extname]`;
+          }
+          return `assets/[name][extname]`;
+        },
+      },
+    },
+  },
+});
