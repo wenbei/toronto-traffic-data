9d6a714cdf88fefd83c98b7ba863084d535da5fb
Wen Bei Li <wenbei999@hotmail.com>
2023-01-14 22:53:29 -0800

    Add count date to TMC view

--- src/ui/ViewCount.svelte
+++ src/ui/ViewCount.svelte
@@ -7,8 +7,8 @@
   import { pop } from "svelte-spa-router";
 
   import { getCountData } from "src/api/toronto-open-data";
-  import TMC from "src/ui/TMC.svelte";
   import { getPeakHourData } from "src/data/processCount";
+  import TMC from "src/ui/TMC.svelte";
 
   let location: string;
   let countData = getCountData(params.id, params.date).then((data) => {
@@ -20,7 +20,7 @@
 <div class="absolute top-0 w-full h-full z-10 py-2 bg-white flex flex-col">
   <div>
     <button on:click={pop} class="cursor-pointer p-1 px-2 border-2 border-black text-lg"> ⮜ Back </button>
-    <h3 class="inline-block text-lg">{location}</h3>
+    <h3 class="inline-block text-lg">{location} ({params.date})</h3>
   </div>
 
   <div class="flex flex-wrap justify-start content-start flex-grow py-2 gap-4">
