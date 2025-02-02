dbc698b8b19521bd9029bdf8d0483552b3051f77
Wen Bei Li <wenbei999@hotmail.com>
2025-02-01 21:52:36 -0800

    Use runes mode

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,15 @@ export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess(),
+  compilerOptions: {
+    runes: true,
+  },
+  // disable runes for svelte-spa-router, https://github.com/ItalyPaleAle/svelte-spa-router/issues/318
+  vitePlugin: {
+    dynamicCompileOptions({ filename }) {
+      if (filename.includes("node_modules")) {
+        return { runes: false };
+      }
+    },
+  },
 };
