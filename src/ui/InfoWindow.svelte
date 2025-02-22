87a5fea294254ec3f85fd4232efc03c4916bc7d1
Wen Bei Li <wenbei999@hotmail.com>
2025-02-21 17:09:39 -0800

    Update Github Actions

--- src/ui/InfoWindow.svelte
+++ src/ui/InfoWindow.svelte
@@ -1,29 +1,26 @@
 <script lang="ts">
-  import { push } from "svelte-spa-router";
+  import type { CountMetadata, LatestCountMetadata } from "src/api/toronto-open-data";
 
-  import type { CountMetadata } from "src/api/toronto-open-data";
+  // import { push } from "svelte-spa-router";
 
-  export let location: string;
-  export let location_id: number;
-  export let countList: CountMetadata[];
+  const { counts, location }: { counts: CountMetadata[]; location: LatestCountMetadata } = $props();
 
   const getWeekday = (date: string) => {
     return new Date(date).toLocaleDateString("en-CA", {
       weekday: "short",
-      timeZone: "UTC",
     });
   };
 </script>
 
-<div class="font-bold">{location} (ID: {location_id})</div>
-<div class="my-1">Available Counts (YYYY-MM-DD):</div>
+<div class="font-bold">{location.location_name}</div>
+<div class="my-1">Available Counts:</div>
 
-<div class="flex flex-col-reverse overflow-auto max-h-40">
-  <ul class="list-disc list-inside">
-    {#each countList as count}
-      <li class="list-item m-1">
+<div class="flex max-h-30 flex-col overflow-auto">
+  <ul class="list-inside list-disc">
+    {#each counts as count}
+      <li class="m-1 list-item">
         {count.count_date} ({getWeekday(count.count_date)})
-        <button on:click={() => push(`#/count/${count.count_id}/${count.count_date}`)} class="mx-2 px-2 py-1 rounded bg-gray-200"> View </button>
+        <!-- <button on:click={() => push(`#/count/${count.count_id}/${count.count_date}`)} class="mx-2 rounded bg-gray-200 px-2 py-1"> View </button> -->
       </li>
     {/each}
   </ul>
--- src/ui/TMC.svelte
+++ src/ui/TMC.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import type { CountData } from "src/api/toronto-open-data";
 
-  export let volumes: CountData & { PHF: number };
+  const volumes: CountData & { PHF: number } = $props();
 </script>
 
 <div class="relative grid aspect-square min-w-[250px] grid-cols-2 grid-rows-2">
@@ -10,60 +10,59 @@
   </div>
   <div class="flex flex-col-reverse gap-2 p-1 outline">
     <div class="flex flex-row-reverse">
-      <span class="vertical text-lg">🡿 {volumes.s_appr_cars_l + volumes.s_appr_truck_l + volumes.s_appr_bus_l}</span>
-      <span class="vertical text-lg">🡸 {volumes.s_appr_cars_t + volumes.s_appr_truck_t + volumes.s_appr_bus_t}</span>
-      <span class="vertical text-lg">🡼 {volumes.s_appr_cars_r + volumes.s_appr_truck_r + volumes.s_appr_bus_r}</span>
+      <span class="vertical text-lg">{volumes.n_appr_cars_l + volumes.n_appr_truck_l + volumes.n_appr_bus_l} 🡽</span>
+      <span class="vertical text-lg">{volumes.n_appr_cars_t + volumes.n_appr_truck_t + volumes.n_appr_bus_t} 🡺</span>
+      <span class="vertical text-lg">{volumes.n_appr_cars_r + volumes.n_appr_truck_r + volumes.n_appr_bus_r} 🡾</span>
     </div>
     <div class="flex flex-row-reverse">
-      <span class="vertical text-lg">({Math.round(((volumes.s_appr_truck_l + volumes.s_appr_bus_l) / (volumes.s_appr_cars_l + volumes.s_appr_truck_l + volumes.s_appr_bus_l)) * 100)}%)</span>
-      <span class="vertical text-lg">({Math.round(((volumes.s_appr_truck_t + volumes.s_appr_bus_t) / (volumes.s_appr_cars_t + volumes.s_appr_truck_t + volumes.s_appr_bus_t)) * 100)}%)</span>
-      <span class="vertical text-lg">({Math.round(((volumes.s_appr_truck_r + volumes.s_appr_bus_r) / (volumes.s_appr_cars_r + volumes.s_appr_truck_r + volumes.s_appr_bus_r)) * 100)}%)</span>
+      <span class="vertical text-lg">({Math.round(((volumes.n_appr_truck_l + volumes.n_appr_bus_l) / (volumes.n_appr_cars_l + volumes.n_appr_truck_l + volumes.n_appr_bus_l)) * 100)}%)</span>
+      <span class="vertical text-lg">({Math.round(((volumes.n_appr_truck_t + volumes.n_appr_bus_t) / (volumes.n_appr_cars_t + volumes.n_appr_truck_t + volumes.n_appr_bus_t)) * 100)}%)</span>
+      <span class="vertical text-lg">({Math.round(((volumes.n_appr_truck_r + volumes.n_appr_bus_r) / (volumes.n_appr_cars_r + volumes.n_appr_truck_r + volumes.n_appr_bus_r)) * 100)}%)</span>
     </div>
   </div>
 
   <div class="flex gap-2 p-1 outline">
     <div class="flex flex-col-reverse">
-      <span class="text-lg">🡿 {volumes.w_appr_cars_l + volumes.w_appr_truck_l + volumes.w_appr_bus_l}</span>
-      <span class="text-lg">🡸 {volumes.w_appr_cars_t + volumes.w_appr_truck_t + volumes.w_appr_bus_t}</span>
-      <span class="text-lg">🡼 {volumes.w_appr_cars_r + volumes.w_appr_truck_r + volumes.w_appr_bus_r}</span>
+      <span class="text-lg">🡿 {volumes.e_appr_cars_l + volumes.e_appr_truck_l + volumes.e_appr_bus_l}</span>
+      <span class="text-lg">🡸 {volumes.e_appr_cars_t + volumes.e_appr_truck_t + volumes.e_appr_bus_t}</span>
+      <span class="text-lg">🡼 {volumes.e_appr_cars_r + volumes.e_appr_truck_r + volumes.e_appr_bus_r}</span>
     </div>
     <div class="flex flex-col-reverse">
-      <span class="text-lg">({Math.round(((volumes.w_appr_truck_l + volumes.w_appr_bus_l) / (volumes.w_appr_cars_l + volumes.w_appr_truck_l + volumes.w_appr_bus_l)) * 100)}%)</span>
-      <span class="text-lg">({Math.round(((volumes.w_appr_truck_t + volumes.w_appr_bus_t) / (volumes.w_appr_cars_t + volumes.w_appr_truck_t + volumes.w_appr_bus_t)) * 100)}%)</span>
-      <span class="text-lg">({Math.round(((volumes.w_appr_truck_r + volumes.w_appr_bus_r) / (volumes.w_appr_cars_r + volumes.w_appr_truck_r + volumes.w_appr_bus_r)) * 100)}%)</span>
-    </div>
-  </div>
-
-  <div class="flex flex-row-reverse gap-2 p-1 outline">
-    <div class="flex flex-col text-right">
-      <span class="text-lg">{volumes.e_appr_cars_l + volumes.e_appr_truck_l + volumes.e_appr_bus_l} 🡽</span>
-      <span class="text-lg">{volumes.e_appr_cars_t + volumes.e_appr_truck_t + volumes.e_appr_bus_t} 🡺</span>
-      <span class="text-lg">{volumes.e_appr_cars_r + volumes.e_appr_truck_r + volumes.e_appr_bus_r} 🡾</span>
-    </div>
-    <div class="flex flex-col text-right">
       <span class="text-lg">({Math.round(((volumes.e_appr_truck_l + volumes.e_appr_bus_l) / (volumes.e_appr_cars_l + volumes.e_appr_truck_l + volumes.e_appr_bus_l)) * 100)}%)</span>
       <span class="text-lg">({Math.round(((volumes.e_appr_truck_t + volumes.e_appr_bus_t) / (volumes.e_appr_cars_t + volumes.e_appr_truck_t + volumes.e_appr_bus_t)) * 100)}%)</span>
       <span class="text-lg">({Math.round(((volumes.e_appr_truck_r + volumes.e_appr_bus_r) / (volumes.e_appr_cars_r + volumes.e_appr_truck_r + volumes.e_appr_bus_r)) * 100)}%)</span>
     </div>
   </div>
 
+  <div class="flex flex-row-reverse gap-2 p-1 outline">
+    <div class="flex flex-col text-right">
+      <span class="text-lg">{volumes.w_appr_cars_l + volumes.w_appr_truck_l + volumes.w_appr_bus_l} 🡽</span>
+      <span class="text-lg">{volumes.w_appr_cars_t + volumes.w_appr_truck_t + volumes.w_appr_bus_t} 🡺</span>
+      <span class="text-lg">{volumes.w_appr_cars_r + volumes.w_appr_truck_r + volumes.w_appr_bus_r} 🡾</span>
+    </div>
+    <div class="flex flex-col text-right">
+      <span class="text-lg">({Math.round(((volumes.w_appr_truck_l + volumes.w_appr_bus_l) / (volumes.w_appr_cars_l + volumes.w_appr_truck_l + volumes.w_appr_bus_l)) * 100)}%)</span>
+      <span class="text-lg">({Math.round(((volumes.w_appr_truck_t + volumes.w_appr_bus_t) / (volumes.w_appr_cars_t + volumes.w_appr_truck_t + volumes.w_appr_bus_t)) * 100)}%)</span>
+      <span class="text-lg">({Math.round(((volumes.w_appr_truck_r + volumes.w_appr_bus_r) / (volumes.w_appr_cars_r + volumes.w_appr_truck_r + volumes.w_appr_bus_r)) * 100)}%)</span>
+    </div>
+  </div>
+
   <div class="flex flex-col gap-2 p-1 outline">
     <div class="flex text-right">
-      <span class="vertical text-lg">{volumes.n_appr_cars_l + volumes.n_appr_truck_l + volumes.n_appr_bus_l} 🡽</span>
-      <span class="vertical text-lg">{volumes.n_appr_cars_t + volumes.n_appr_truck_t + volumes.n_appr_bus_t} 🡺</span>
-      <span class="vertical text-lg">{volumes.n_appr_cars_r + volumes.n_appr_truck_r + volumes.n_appr_bus_r} 🡾</span>
+      <span class="vertical text-lg">🡿 {volumes.s_appr_cars_l + volumes.s_appr_truck_l + volumes.s_appr_bus_l}</span>
+      <span class="vertical text-lg">🡸 {volumes.s_appr_cars_t + volumes.s_appr_truck_t + volumes.s_appr_bus_t}</span>
+      <span class="vertical text-lg">🡼 {volumes.s_appr_cars_r + volumes.s_appr_truck_r + volumes.s_appr_bus_r}</span>
     </div>
     <div class="flex text-right">
-      <span class="vertical text-lg">({Math.round(((volumes.n_appr_truck_l + volumes.n_appr_bus_l) / (volumes.n_appr_cars_l + volumes.n_appr_truck_l + volumes.n_appr_bus_l)) * 100)}%)</span>
-      <span class="vertical text-lg">({Math.round(((volumes.n_appr_truck_t + volumes.n_appr_bus_t) / (volumes.n_appr_cars_t + volumes.n_appr_truck_t + volumes.n_appr_bus_t)) * 100)}%)</span>
-      <span class="vertical text-lg">({Math.round(((volumes.n_appr_truck_r + volumes.n_appr_bus_r) / (volumes.n_appr_cars_r + volumes.n_appr_truck_r + volumes.n_appr_bus_r)) * 100)}%)</span>
+      <span class="vertical text-lg">({Math.round(((volumes.s_appr_truck_l + volumes.s_appr_bus_l) / (volumes.s_appr_cars_l + volumes.s_appr_truck_l + volumes.s_appr_bus_l)) * 100)}%)</span>
+      <span class="vertical text-lg">({Math.round(((volumes.s_appr_truck_t + volumes.s_appr_bus_t) / (volumes.s_appr_cars_t + volumes.s_appr_truck_t + volumes.s_appr_bus_t)) * 100)}%)</span>
+      <span class="vertical text-lg">({Math.round(((volumes.s_appr_truck_r + volumes.s_appr_bus_r) / (volumes.s_appr_cars_r + volumes.s_appr_truck_r + volumes.s_appr_bus_r)) * 100)}%)</span>
     </div>
   </div>
 </div>
 
 <style>
   span.vertical {
-    writing-mode: vertical-lr;
-    transform: scale(-1);
+    writing-mode: vertical-rl;
   }
 </style>
